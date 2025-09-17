e1367535b4b9e5d2a02ec24f63a599779be1268e
David Dada <daveproxy80@gmail.com>
2025-09-17 17:44:01 +0100

    chore: add get all circles view function

--- src/Rosca.sol
+++ src/Rosca.sol
@@ -561,6 +561,13 @@ contract RoscaSecure is ReentrancyGuard, Pausable, Ownable {
         return circles[circleId];
     }
 
+    function getAllCircles() external view returns (Circle[] memory circles_) {
+        circles_ = new Circle[](nextCircleId);
+        for (uint256 i = 1; i < nextCircleId; ++i) {
+            circles_[i] = _getCircle(i);
+        }
+    }
+
     function _getCircleDetails(uint256 circleId) private view returns (CircleDetails memory) {
         return circleDetails[circleId];
     }
